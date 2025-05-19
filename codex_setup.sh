771218d2a9548b228c7a4c2699de453d45ca0d6e
Edward Z. Yang <ezyang@meta.com>
2025-05-18 21:57:47 -0400

    Handle fact that source doesn't work
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>

--- codex_setup.sh
+++ codex_setup.sh
@@ -1,4 +1,2 @@
-# This file is source'd by the codex agent and so any env vars it sets are
-# available to the agent
 uv sync
-source .venv/bin/activate
+echo "source .venv/bin/activate" >> ~/.bashrc
